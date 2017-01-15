5fd910d3a29317e4bf59e6c0b794932713c51c1c
james <james@nwshire.net>
2017-01-15 15:59:47 -0800

    Tightened up

--- src/com/nwshire/leetcode/RegExpA.java
+++ src/com/nwshire/leetcode/RegExpA.java
@@ -8,35 +8,35 @@ import java.util.List;
  */
 public class RegExpA {
     public boolean isMatch(String s, String p) {
-        PatternPart pps[] = loadPattern(p);
-        return match(s, pps, 0);
+        return match(s, p);
     }
 
-    private boolean match(String s, PatternPart pps[], int pi) {
+    private boolean match(String s, String p) {
         boolean match = false;
 
-        if ( pi < pps.length ) {
-            PatternPart p = pps[pi];
+        if ( p.length() > 0 ) {
+            char value = p.charAt(0);
+            boolean wildcard = p.length() > 1 && p.charAt(1) == '*';
 
-            if ( p.wildcard ) {
+            if ( wildcard ) {
                 if ( s.length() > 0 ) {
                     for ( int l=0; l<=s.length(); l++ ) {
-                        match = l == 0 || p.charMatches(s.charAt(l-1));
+                        match = l == 0 || (value == '.' || value == s.charAt(l-1));
                         if ( !match ) {
                             break; // wildcard exhausted
                         }
 
-                        match = match(s.substring(l), pps, pi+1);
+                        match = match(s.substring(l), p.substring(2));
                         if ( match ) {
                             break; // match found
                         }
                     }
                 } else {
-                    match = match(s, pps, pi+1); // zero or more.
+                    match = match(s, p.substring(2)); // zero or more.
                 }
             } else {
                 if ( s.length() > 0 ) {
-                    match = p.charMatches(s.charAt(0)) && match(s.substring(1), pps, pi+1);
+                    match = (value == '.' || value == s.charAt(0)) && match(s.substring(1), p.substring(1));
                 }
             }
         } else {
@@ -44,33 +44,5 @@ public class RegExpA {
         }
 
         return match;
-    }
-
-    private PatternPart[] loadPattern(String p) {
-        char pc[] = p.toCharArray();
-        List<PatternPart> pps = new ArrayList<>();
-
-        for ( int n=0; n < pc.length; ) {
-            boolean wildcard = n+1 < pc.length && pc[n+1] == '*';
-
-            pps.add(new PatternPart(pc[n], wildcard));
-            n = n + ( wildcard ? 2 : 1 );
-        }
-
-        return pps.toArray(new PatternPart[0]);
-    }
-
-    private class PatternPart {
-        char value;
-        boolean wildcard;
-
-        PatternPart(char value, boolean wildcard) {
-            this.value = value;
-            this.wildcard = wildcard;
-        }
-
-        public boolean charMatches(char c) {
-            return value == '.' || value == c;
-        }
     }
 }

cab78364ff780f1916cddf5ab0cac3ba8556a25b
james <james@nwshire.net>
2017-01-18 17:19:26 -0800

    Implement proper binary search in FindLowest

--- src/com/nwshire/FindLowest.java
+++ src/com/nwshire/FindLowest.java
@@ -13,6 +13,31 @@ public class FindLowest {
 
         if ( arr.length > 0 ) {
             minIndex = 0;
+            int low = 0;
+            int high = arr.length-1;
+
+            while ( low+1 < high ) {
+                int mid = low + ((high - low) / 2);
+                if ( arr[low] > arr[mid] ) {
+                    high = mid;
+                } else if ( arr[mid] > arr[high] ) {
+                    low = mid;
+                } else {
+                    break; // list is not roated.  exit.
+                }
+            }
+
+            minIndex = arr[low] < arr[high] ? low : high;
+        }
+
+        return minIndex;
+    }
+
+    public int findLowestLive(int arr[]) {
+        int minIndex = -1;
+
+        if ( arr.length > 0 ) {
+            minIndex = 0;
             int i = arr.length / 2;
 
             int low = 0;
